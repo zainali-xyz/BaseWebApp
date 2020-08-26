94a9dc5fded7149428e4758a0bc098c4664e8ddf
Zain Ali <me@zainali.xyz>
2020-08-25 20:16:19 -0400

    Changed the text

--- project/js/main.js
+++ project/js/main.js
@@ -2,7 +2,7 @@ function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+  $("p").html("High five! Enjoy some cute images!");
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
